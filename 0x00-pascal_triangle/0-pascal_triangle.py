f01e39f56e3411f216fac24e2bdce310146980a1
maleekmalik@gmail.com <maleekmalik@gmail.com>
2024-01-04 14:47:51 +0300

    0-pascal_triangle.py docstrings

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """
-returns a list of lists of integers representing the Pascal’s triangle
+Returns a list of lists of integers representing Pascal's triangle of n:
+
+Returns an empty list if n <= 0
+You can assume n will be always an integer
 """
 
 
